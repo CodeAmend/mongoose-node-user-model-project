aef7cc65e33430d5b1d8e443d5782d040fae6aec
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-03 12:54:00 -0500

    Added reading_test.js

---
+++ test/reading_test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+const User = require('../src/user');
+
+describe("Reading users out of the database", () => {
+
+  beforeEach( (done) => {
+    joe = new User({name: 'joe'})
+    joe.save()
+      .then( () => done() );
+  });
+
+  it('finds all users as name of joe', (done) => {
+    User.find({name: 'joe'})
+      .then( (users) => {
+        assert(joe._id.toString() === users[0]._id.toString());
+        done();
+      });
+  });
+
+  it('finds a user with a particular name', (done) => {
+    User.findOne({ _id: joe._id })
+      .then( (user) => {
+        assert( user.name === 'joe');
+        done();
+      });
+  });
+});
