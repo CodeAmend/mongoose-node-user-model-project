30d5de8addc947bccbc1f79fc2346e755c21a42c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-03 12:53:11 -0500

    Added create_test.js

---
+++ test/create_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe("Creating records", () => {
+
+  it("saves a user", (done)=> {
+    const joe = new User({name: 'Joe' });
+    assert(joe.isNew);
+    joe.save()
+      .then( () => {
+        assert(!joe.isNew);
+        done()
+      });
+    // assert(joe.isNew === false);
+  });
+});
