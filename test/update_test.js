796bea9e904fdf3a0e491980d764ad834e0ae9c5
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-03 12:54:32 -0500

    Added update_test.js

---
+++ test/update_test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe("Updating records", () => {
+  let joe;
+
+  beforeEach( (done) => {
+    joe = new User({name: "Joe"})
+    joe.save()
+      .then( () => done() );
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then( () => User.find({}))
+      .then( (users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'alex');
+        done();
+    });
+  }
+
+  it("intance type using set and save", (done) => {
+    joe.set("name", "alex");
+    assertName(
+      joe.save(), done);
+
+  });
+  // one go... no set AND save
+  it("model instance can update", (done) => {
+    assertName(
+      joe.update({name: 'alex'}), done);
+  });
+
+});
